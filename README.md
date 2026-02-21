5d8a8a97ba9959ca26ea93e29bf102d871b8d06d
yishaqsilva <yishaqsilva@gmail.com>
2026-02-21 21:11:03 +0100

    multiple argument support

--- README.md
+++ README.md
@@ -79,6 +79,7 @@ Take control of your CPU — just freeze and unfreeze.
 Suspends the application and all its sub-processes instantly.
 ```shell
 cryo freeze firefox
+cryo freeze firefox vlc obs
 ```
 
 🥵 Unfreeze
@@ -86,6 +87,7 @@ cryo freeze firefox
 Brings the application back exactly where you left off.
 ```shell
 cryo unfreeze firefox
+cryo unfreeze firefox vlc obs
 ```
 
 🖥️ Show
@@ -102,6 +104,7 @@ Shows the status of the selected process
 
 ```shell
 cryo status firefox
+cryo status firefox vlc obs
 ```
 ---
 
--- cryo.py
+++ cryo.py
@@ -4,134 +4,137 @@ import os
 import argparse
 
 def if_safe(proc):
-	try:
-		if proc.pid < 1000 or proc.pid == os.getpid() or proc.pid == os.getppid():
-			return False
-		return True
-	except:
-		return False
+    try:
+        if proc.pid < 1000 or proc.pid == os.getpid() or proc.pid == os.getppid():
+            return False
+        return True
+    except:
+        return False
 
 def show_all_procs():
-	print("~~~ Active processes ~~~")
-	for proc in psutil.process_iter(['pid', 'name']):
-		try:
-			if proc.info['pid'] < 1000:
-				continue
-			print(f"{proc.info['pid']} - {proc.info['name']}")
-		except:
-			pass
+    print("~~~ Active processes ~~~")
+    for proc in psutil.process_iter(['pid', 'name']):
+        try:
+            if proc.info['pid'] < 1000:
+                continue
+            print(f"{proc.info['pid']} - {proc.info['name']}")
+        except:
+            pass
 
 def proc_status(name):
-	proc = find_pid(name)
-	if proc.status() == psutil.STATUS_STOPPED:
-		print("Process is frozen ❄️")
-	else:
-		print("Process is running 🟢")
+    proc = find_pid(name)
+    if proc.status() == psutil.STATUS_STOPPED:
+        print(f"Process '{name}' is frozen ❄️")
+    else:
+        print(f"Process '{name}' is running 🟢")
 
 
 def find_pid(name):
-	for proc in psutil.process_iter(['pid', 'name']):
-		try:
-			if name == proc.info['name']:
-				return proc
-		except:
-			continue
-	print("Cannot find process " + name)
-	return None
+    for proc in psutil.process_iter(['pid', 'name']):
+        try:
+            if name == proc.info['name']:
+                return proc
+        except:
+            continue
+    print("Cannot find process " + name)
+    return None
 
 def find_children(name):
-	pid_parent = find_pid(name)
-	if not pid_parent:
-		print("Process " + name + " doesn't exist")
-		return []
-	try:
-		children = pid_parent.children(recursive=True)
-		return [*[pid_parent], *list(children)]
-	except:
-		print("Cannot find children for " + name)
-		return []
+    pid_parent = find_pid(name)
+    if not pid_parent:
+        print("Process " + name + " doesn't exist")
+        return []
+    try:
+        children = pid_parent.children(recursive=True)
+        return [*[pid_parent], *list(children)]
+    except:
+        print("Cannot find children for " + name)
+        return []
 
 def main():
-	parser = argparse.ArgumentParser(
-		description="Cryo: Process Freezer. Stop apps without closing them.",
-		epilog="Example: cryo freeze firefox"
-	)
+    parser = argparse.ArgumentParser(
+        description="Cryo: Process Freezer. Stop apps without closing them.",
+        epilog="Example: cryo freeze firefox\ncryo freeze firefox vlc"
+    )
 
-	subparsers = parser.add_subparsers(
-		dest="command",
-		required=True,
-		help="Available commands"
-	)
+    subparsers = parser.add_subparsers(
+        dest="command",
+        required=True,
+        help="Available commands"
+    )
 
-	freeze_parser = subparsers.add_parser(
-		"freeze",
-		help="Freeze a process tree"
-	)
-	freeze_parser.add_argument(
-		"name",
-		type=str,
-		help="Process name (e.g., firefox)"
-	)
+    freeze_parser = subparsers.add_parser(
+        "freeze",
+        help="Freeze a process tree"
+    )
+    freeze_parser.add_argument(
+        "name",
+        type=str,
+        nargs="*",
+        help="Process name (e.g., firefox)"
+    )
 
-	unfreeze_parser = subparsers.add_parser(
-		"unfreeze",
-		help="Unfreeze a process tree"
-	)
-	unfreeze_parser.add_argument(
-		"name",
-		type=str,
-		help="Process name (e.g., firefox)"
-	)
+    unfreeze_parser = subparsers.add_parser(
+        "unfreeze",
+        help="Unfreeze a process tree"
+    )
+    unfreeze_parser.add_argument(
+        "name",
+        type=str,
+        nargs="*",
+        help="Process name (e.g., firefox)"
+    )
 
-	status_parser = subparsers.add_parser(
-		"status",
-		help="Show process status (e.g freeze/running)"
-	)
+    status_parser = subparsers.add_parser(
+        "status",
+        help="Show process status (e.g freeze/running)"
+    )
 
-	status_parser.add_argument(
-		"name",
-		type=str,
-		help="Process name (e.g., firefox)"
-	)
+    status_parser.add_argument(
+        "name",
+        type=str,
+        nargs="*",
+        help="Process name (e.g., firefox)"
+    )
 
-	show_procs = subparsers.add_parser(
-		"show",
-		help="Show all active processes"
-	)
+    show_procs = subparsers.add_parser(
+        "show",
+        help="Show all active processes"
+    )
 
-	try:
-		args = parser.parse_args()
-	except:
-		parser.print_help()
-		return
+    try:
+        args = parser.parse_args()
+    except:
+        parser.print_help()
+        return
 
-	if args.command == "show":
-		show_all_procs()
-		return
-	elif args.command == "status":
-		proc_status(args.name)
-		return
+    if args.command == "show":
+        show_all_procs()
+        return
+    elif args.command == "status":
+        for name in args.name:
+            proc_status(name)
+        return
+    
+    for name in args.name:
+        procs = find_children(name)
+        if not procs:
+            print("We didn't find any processes for " + name)
+            return
 
+        for proc in procs:
+            if not if_safe(proc):
+                print(f"System critical process {proc}!!!")
+                continue
+            try:
+                if args.command == "freeze":
+                    proc.suspend()
+                elif args.command == "unfreeze":
+                    proc.resume()
+            except:
+                pass
 
-	name = args.name
-	procs = find_children(name)
-	if not procs:
-		print("We didn't find any processes for " + name)
-		return
-
-	for proc in procs:
-		if not if_safe(proc):
-			print(f"System critical process {proc}!!!")
-			continue
-		try:
-			if args.command == "freeze":
-				proc.suspend()
-			elif args.command == "unfreeze":
-				proc.resume()
-		except:
-			pass
-
-	print("Completed successfully!")
+    print("Completed successfully!")
 
 if __name__ == "__main__":
-	main()
+    main()
